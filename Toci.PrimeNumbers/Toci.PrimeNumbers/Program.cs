ce47662f5eed52a38f1ad88a06d667fde73e332c
Bartomiej Zapart <bartlomiej.zapart@argosmultilingual.com>
2021-05-02 13:29:40 +0200

    The Toci Prime Number Generator !!! xD

--- Toci.PrimeNumbers/Toci.PrimeNumbers/Program.cs
+++ Toci.PrimeNumbers/Toci.PrimeNumbers/Program.cs
@@ -1,20 +1,25 @@
-﻿using System;
+﻿//using Azure.Storage.Files.Shares.Models;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 
 namespace Toci.PrimeNumbers
 {
     class Program
     {
+        private const UInt64 V = 11000000000000000000;
         static UInt64 Two = 2;
-        static UInt64 MinusTwo = 0;
+        static UInt64 MinusTwo = 2;
         static UInt64 Four = 4;
         static UInt64 Six = 6;
         static UInt64 Eight = 8;
         static UInt64 Ten = 10;
         static UInt64 N = 11;
         static int Length = 3;
+        static StringBuilder stringBuilder = new StringBuilder();
+        //static Azure.Storage.Files.Shares.ShareClient client = new Azure.Storage.Files.Shares.ShareClient("", "PrimeNumerToci.txt");
 
         static void Main(string[] args)
         {
@@ -27,20 +32,15 @@ namespace Toci.PrimeNumbers
                 { 5, (n) => n + Six },
                 { 6, (n) => n + Two },
                 { 7, (n) => n + Six },
-                { 8, (n) => n + Six },
-                { 9, (n) => n + Four },
-                { 10, (n) => n + Six },
+                { 8, (n) => n - MinusTwo },
+                { 9, (n) => n + Six },
+                { 10, (n) => n + Two },
                 { 11, (n) => n + Six },
                 { 12, (n) => n + Two },
-                { 13, (n) => n + Six },
-                { 14, (n) => n + Six },
-                { 15, (n) => n + Four },
-                { 16, (n) => n + Six },
-                { 17, (n) => n + Six },
-                { 18, (n) => n + Two },
-                { 19, (n) => n + Six },
-                { 20, (n) => n + Four },
+
             };
+
+            //Azure.Response<ShareInfo> stream = client.Create(new ShareCreateOptions());
 
             StreamWriter swr = new StreamWriter(@"D:\Toci\Primes.txt");
 
@@ -53,8 +53,7 @@ namespace Toci.PrimeNumbers
                         Length = N.ToString().Length;
 
                         Two = 2 + Two * 10;
-                        MinusTwo = 0;
-                        //MinusTwo = 2 + MinusTwo * 10;
+                        MinusTwo = 2 + MinusTwo * 10;
                         Four = 4 + Four * 10;
                         Six = 6 + Six * 10;
                         Eight = 8 + Eight * 10;
@@ -63,16 +62,18 @@ namespace Toci.PrimeNumbers
 
                     try
                     {
-   
-                            N = next.Value(N);
-                            SaveToFile(N, swr);
-                        
-                        
+                        N = next.Value(N);
+
+                        if (N.ToString().Length > 19)
+                        {
+                            N = N | V;
+                            SaveToFile(N.ToString().Substring(1, 1), swr);
+                            //stream.GetRawResponse().ContentStream.Write(UTF8Encoding.GetEncoding(852).GetBytes(N.ToString().Substring(1, 1)));
+                        }
                     }
                     catch
                     {
                         swr.Close();
-                        return;
                     }
                 }
             }
@@ -80,7 +81,13 @@ namespace Toci.PrimeNumbers
 
         static void SaveToFile(UInt64 N, StreamWriter swr)
         {
-            swr.WriteLine(N);
+            swr.Write(N);
+        }
+
+        static void SaveToFile(string stringBuilder, StreamWriter swr)
+        {
+
+            swr.Write(stringBuilder);
         }
     }
 }
